bf8e4279c75c65e22036144ba481df131a15569b
voidbert <50591320+voidbert@users.noreply.github.com>
2023-08-09 19:20:38 +0000

    Error handlers

--- powermodes/error.py
+++ powermodes/error.py
@@ -23,6 +23,7 @@
 
 from enum import Enum
 from sys import stderr
+from typing import Union
 
 ##
 # @brief Type of an [Error](@ref powermodes.error.Error).
@@ -65,3 +66,58 @@ def print_error(err: Error) -> None:
 
     print(print_str, file=stderr)
 
+##
+# @brief Handle errors from functions that may return them.
+# @details
+# Prints errors and leaves the program on fatal errors.
+#
+# Used for functions with one of the following type signatures:
+#  - `fn() -> tuple[Union[any, None], Union[Error, None]]`
+#  - `fn() -> tuple[Union[any, None], list[Error]]`
+#
+# If the error isn't `None`, it will be printed. If there are non-warning errors and the
+# returned object is `None`, the program is left fatally.
+#
+# @param output Return value from the function that may return an error.
+# @returns The value returned by the function whose result is placed in @p output.
+##
+def handle_error(output: tuple[Union[any, None], Union[Error, list[Error], None]]) -> any:
+    obj, err = output
+    has_errors = False
+    if type(err) == Error:
+        print_error(err)
+        has_errors = err.error_type == ErrorType.ERROR
+    elif type(err) == list:
+        for e in err:
+            print_error(e)
+        has_errors = any(map(lambda e: e.error_type == ErrorType.ERROR, err))
+    else:
+        has_errors = False
+
+    if obj is None and has_errors:
+        exit(1)
+
+    return obj
+
+##
+# @brief Handles errors by appending them to a list.
+# @details
+# Used for functions with one of the following type signatures:
+#  - `fn() -> tuple[Union[any, None], Union[Error, None]]`
+#  - `fn() -> tuple[Union[any, None], list[Error]]`
+#
+# Any error will be appended @p lst and no error will be printed.
+#
+# @param lst List of errors to which to append new errors.
+# @param output Return value from the function that may return an error.
+# @returns The value returned by the function whose result is placed in @p output.
+##
+def handle_error_append(lst: list[Error], \
+                        output: tuple[Union[any, None], Union[Error, list[Error], None]]) -> any:
+    obj, err = output
+    if type(err) == Error:
+        lst.append(err)
+    elif type(err) == list:
+        lst.extend(err)
+
+    return obj
