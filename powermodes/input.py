95fc115906312fe9ecc1d1f8400a7ec93fd06141
voidbert <50591320+voidbert@users.noreply.github.com>
2023-08-22 23:00:20 +0000

    input.py: add examples to documentation

--- powermodes/input.py
+++ powermodes/input.py
@@ -21,6 +21,9 @@ powermodes.input
 
 Methods to read and parse user input. Useful for powermodes' interactive mode
 (:attr:`powermodes.arguments.Action.INTERACTIVE`) and for any plugin's interactive features.
+
+Module contents
+^^^^^^^^^^^^^^^
 """
 
 from collections.abc import Iterable
@@ -28,7 +31,9 @@ from typing import Any
 from sys import maxsize, stderr
 
 def input_integer(bottom: int = -maxsize - 1, top: int = maxsize,
-                  prompt: str ='{bottom} - {top} > ') -> int:
+                  prompt: str ='{bottom} - {top} > ', \
+                  error: str = 'Input must be an integer between {bottom} and {top}!') \
+                -> int:
     """Queries the user to input an integer, then reads it from ``stdin``. In case the text
     inputted does not read as an integer in the desired range, an error message is printed to
     ``stdout`` and this method tries to get an integer from the user again and again, until valid
@@ -40,8 +45,23 @@ def input_integer(bottom: int = -maxsize - 1, top: int = maxsize,
                 largest integer for the user's system (``sys.maxsize``).
     :param prompt: Prompt provided to Python's ``input`` method. The string will be formatted with
                the values of arguments ``bottom`` and ``top``.
+    :param error: Error message for when the user's input isn't an integer between ``bottom`` and
+                  ``top``. The string will be formatted with the values of arguments ``bottom`` and
+                  ``top``.
 
     :return: The integer inputted by the user.
+
+    Example:
+
+    .. code:: python
+
+        >>> input_integer(0, 10, 'Integer from {bottom} to {top} > ', \\
+                'Come on, that\\\'s not what I asked for!')
+
+        # Integer from 0 to 10 > 11
+        # Come on, that\'s not what I asked for!
+        # Integer from 0 to 10 > 7
+        # 7
     """
 
     while True:
@@ -53,7 +73,7 @@ def input_integer(bottom: int = -maxsize - 1, top: int = maxsize,
             else:
                 raise ValueError()
         except ValueError:
-            print(f'Input must be an integer between {bottom} and {top}.', file=stderr)
+            print(error.format(bottom=bottom, top=top), file=stderr)
 
 def print_options(options: Iterable[str],
                   message: str ='Choose an option:', line_format: str ='  ({n}) - {opt}') -> None:
@@ -67,6 +87,16 @@ def print_options(options: Iterable[str],
     :param line_format: The string used to format each line containing an option. It will be
                         formatted variables with ``n``, the number of the option, and ``opt``, the
                         text content of the option in ``options``.
+
+    Example:
+
+    .. code:: python
+
+        >>> print_options(['Alice', 'Bob', 'Charlie'], 'Choose who to hack:')
+        Choose who to hack:
+          (1) - Alice
+          (2) - Bob
+          (3) - Charlie
     """
 
     print(message, end='\n')
@@ -77,7 +107,8 @@ def print_options(options: Iterable[str],
 
 def choose_option(options: list[tuple[Any, str]], \
                   message: str ='Choose an option:', line_format: str ='  ({n}) - {opt}', \
-                  prompt: str ='{bottom} - {top} > ') -> Any:
+                  prompt: str ='{bottom} - {top} > ', \
+                  error: str = 'Input must be an integer between {bottom} and {top}!') -> Any:
     """Asks the user to choose an item from a list of options, by inputting a number. Like
     :func:`input_integer`, this function will keep trying to read input until a valid integer is
     read.
@@ -96,8 +127,25 @@ def choose_option(options: list[tuple[Any, str]], \
     :param prompt: Argument provided to :func:`input_integer`: Prompt provided to Python's
                    ``input`` method. The string will be formatted with the values of
                    ``bottom`` (1) and ``top`` (``1 + len(options)``).
+    :param error: Argument provided to :func:`input_integer`. Error message for when the user's
+                  input isn't an integer between ``bottom`` and ``top``. The string will be
+                  formatted with the values of arguments ``bottom`` and ``top``.
 
     :return: The first element of one of the tuples in ``options``.
+
+    Example:
+
+    .. code::  python
+
+        >>> choose_option([('192.168.1.2', 'Alice'), ('192.168.1.3', 'Bob')], \\
+                'Choose who to hack:')
+        # Choose who to hack:
+        #  (1) - Alice
+        #  (2) - Bob
+        # 1 - 2 > 8
+        # Input must be an integer between 1 and 2!
+        # 1 - 2 > 1
+        # '192.168.1.2'
     """
 
     print_options(map(lambda t: t[1], options), message, line_format)
